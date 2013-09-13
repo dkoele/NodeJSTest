4442c173b75c801a8de9a5e67dce3f18f750a0e7
dkoele <daniel@koele.eu>
2013-09-13 16:27:26 +0200

    added pi

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function(req,resp) {
 	resp.writeHead(200, {"Content-Type": "text/plain"});
-	resp.write("Node was set up successfully on your Raspberry.");
+	resp.write("Node was set up successfully on your Raspberry Pi.");
 	resp.end();
 	
 	console.log("sample output to console");
